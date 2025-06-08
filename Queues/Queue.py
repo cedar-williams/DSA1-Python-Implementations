aa3bf5a6177304eae5a641af1a0908e33e8b1d0e
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-06-08 14:43:35 -0400

    Update queue

--- Queues/Queue.py
+++ Queues/Queue.py
@@ -1,18 +1,22 @@
-from Lists import LinkedList
+from Lists.LinkedList import LinkedList
+from Node import Node
 
 class Queue:
     def __init__(self):
-        self.array = []
-        self.length = 0
-        self.front_index = 0
+        self.list = LinkedList()
 
     def enqueue(self, item):
-        pass
+        node = Node(item)
+        self.list.append(node)
 
     def dequeue(self):
-        pass
+        dequeued_item = self.list.head.data
+        self.list.remove_after(None)
+        return dequeued_item
+
 
     def peek(self):
-        pass
+        return self.list.head.data
 
-    def IsEmpty
+    def is_empty(self):
+        return self.list.head is None
