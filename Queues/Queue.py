2de0f24fe142984c476db86fbba7e36c11728700
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-06-08 14:35:41 -0400

    Add Queues

---
+++ Queues/Queue.py
@@ -0,0 +1,18 @@
+from Lists import LinkedList
+
+class Queue:
+    def __init__(self):
+        self.array = []
+        self.length = 0
+        self.front_index = 0
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def IsEmpty
