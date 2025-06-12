9a9e9e03d88bf653c768d3664a74586d1768b7f2
Cedar Williams <33876916+cedar-williams@users.noreply.github.com>
2025-06-12 12:41:44 -0400

    Add testing code

---
+++ B-trees/main.py
@@ -0,0 +1,22 @@
+user_values = input('Enter insert values with spaces between: ')
+print()
+
+# Create a 2-3-4 tree and add the values
+tree = Tree234()
+for value in user_values.split():
+    tree.insert(int(value))
+
+# Display the height after all inserts are complete.
+print("2-3-4 tree with " + str(len(tree)) + " keys has height " + str(tree.get_height()))
+
+# Read user input to get a list of values to remove
+user_values = input('Enter remove values with spaces between: ')
+print()
+
+for value in user_values.split():
+    to_remove = int(value)
+    print('Remove node ' + value + ': ', end='')
+    if tree.remove(to_remove):
+        print('2-3-4 tree with ' + str(len(tree)) + ' keys has height ' + str(tree.get_height()))
+    else:
+        print('*** Key not found. Tree is not changed. ***')
